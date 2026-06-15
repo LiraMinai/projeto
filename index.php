e9daf558058c1b678fb31f505751e259650da29e
Laura <laurafernandessm@gmail.com>
2026-06-15 14:24:24 -0300

    14:24

--- index.php
+++ index.php
@@ -12,5 +12,9 @@
         <img src="logo.png">
         <h4>Cadastre-se e crie seu personagem para que, juntos, possam se aventurar nos estudos.</h4>
     </div>
+    <div class="botoes">
+        <button onclick="window.location.href='entrar.php'">ENTRAR</button>
+        <button onclick="window.location.href='cadastrar.php'">CADASTRE-SE</button>
+    </div>
 </body>
 </html>
