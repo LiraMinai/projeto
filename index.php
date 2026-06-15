d8ef3ea30ce816b001a2f5905db609e9af38a2de
Laura <laurafernandessm@gmail.com>
2026-06-15 14:40:06 -0300

    14:40

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <div class="wave">
         <h1>YDUTS</h1>
         <h2>Pronto para começar sua jornada?</h2>
-        <img src="logo.png">
+        <img src="imagens/logo.png">
         <p>Cadastre-se e crie seu personagem para que, juntos, possam se aventurar nos estudos.</p>
     </div>
     <div class="botoes">
