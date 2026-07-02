41dcc051809372aacd35bd5da64d084a5b959521
Laura Fernandes <laura.merlo@alunos.ifsuldeminas.edu.br>
2026-07-02 14:58:47 -0300

    14:58

--- cadastrar.php
+++ cadastrar.php
@@ -10,7 +10,40 @@
     <h1 class="titulo">YDUTS</h1>
     <h1 class="titulo">ENTRAR</h1>
     <form method="POST">
-        <p class="textodif">
+        <p class="textodif" name="nomeUsuario">Nome</p>
+        <input type="text">
+        <p class="textodif" name="nomeUsuarioUsuario">Nome de usuário</p>
+        <input type="text">
+        <p class="textodif" name="emailUsuario">E-mail</p>
+        <input type="text">
+        <p class="textodif" name="senhaUsuario">Senha</p>
+        <input type="password">
+        <br><br>
+        <p class="textodif">Falta pouco para você começar sua aventura.</p>
+        <br><br>
+        <button type="submit">Prosseguir</button>
     </form>
+    <?php
+        $nomeUsuario = $_POST['nomeUsuario'];
+        $nomeUsuarioUsuario = $_POST['nomeUsuarioUsuario'];
+        $emailUsuario = $_POST['emailUsuario'];
+        $senhaUsuario = $_POST['senhaUsuario'];
+
+        include_once "conexao.php";
+
+        $query = "INSERT INTO usuario (nomeUsuario, nomeUsuarioUsuario, emailUsuario, senhaUsuario) VALUES (?,?,?,?)";
+        $stm = $db->prepare($query);
+        $stm->bindParam(1, $nomeUsuario);
+        $stm->bindParam(2, $nomeUsuarioUsuario);
+        $stm->bindParam(3, $emailUsuario);
+        $stm->bindParam(4, $senhaUsuario);
+
+        if($stm->execute()){
+            header('location:index.php');
+        }
+        else {
+            print "<p>Erro ao inserir</p>";
+        }
+    ?>
 </body>
 </html>
