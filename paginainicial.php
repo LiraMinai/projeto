9b282997238c094af47c155333c3ff7a85efe487
Yasmin <yasmin@gmail.com>
2026-08-10 07:44:47 -0300

    07:44

--- paginainicial.php
+++ paginainicial.php
@@ -46,8 +46,10 @@ $avatar          = $dados["avatarPersonagem"];
         </div>
         <h1 class="tituloCad2">MATÉRIAS</h1>
     </header>
-    <button class="centralizar" onclick="window.location.href='materias/portugues.php'">Português</button>
-    <button class="centralizar" onclick="window.location.href='materias/ingles.php'">Inglês</button>
+<div class="centralizar">
+    <button onclick="window.location.href='materias/portugues.php'">Português</button>
+    <button onclick="window.location.href='materias/ingles.php'">Inglês</button>
+</div>
     <div class="hud">
         <div class="avatar">
             <img src="imagens/personagem.png">
